e575bc362df637665d187366da424da516f173cb
kerrot <kerrot.gs>
2017-07-29 18:18:58 +0900

    init

---
+++ Assets/CamRay.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamRay : MonoBehaviour {
+
+    GameObject obj;
+	
+    public delegate void ClickEvent();
+    public ClickEvent OnClick; 
+
+	// Update is called once per frame
+	void Update () {
+        RaycastHit hit;
+
+		if (Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out hit))
+        {
+            if (hit.collider is CapsuleCollider)
+            {
+                if (obj == null && OnClick != null)
+                {
+                    OnClick();
+                }
+
+                obj = hit.collider.gameObject;
+                obj.GetComponent<MeshRenderer>().material.color = Color.red;
+            }
+        }
+        else
+        {
+            if (obj)
+            {
+                obj.GetComponent<MeshRenderer>().material.color = Color.white;
+                obj = null;
+            }
+        }
+	}
+}
---
+++ Assets/NetworkTest.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class NetworkTest : NetworkManager {
+    CamRay ray;
+
+	// Use this for initialization
+	void Start () {
+        ray = GameObject.FindObjectOfType<CamRay>();
+        ray.OnClick += Connect;
+	}
+
+    void Connect()
+    {
+        StartClient();
+    }
+
+    private void OnDestroy()
+    {
+        if (ray.OnClick != null)
+        {
+            ray.OnClick -= Connect;
+        }
+    }
+}
