cc7411eaca7ff8c7c2316800645c793b1ebea4af
kerrot <kerrot.gs>
2017-07-29 19:23:30 +0900

    Rotote

---
+++ Assets/Rotote.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotote : MonoBehaviour {
+    [SerializeField]
+    private Vector3 angle;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate(angle);
+	}
+}
